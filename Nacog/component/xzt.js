82da151c6f5ee56759474aeee288e8fb7c1338e6
Nacog <88483952+Nacocx@users.noreply.github.com>
2025-05-24 14:47:15 +0800

    remake struct

---
+++ Nacog/component/xzt.js
@@ -0,0 +1,39 @@
+const xzt = {
+  template: `
+   <div>
+    <h1>选择题部分</h1>
+    <div
+      class="question-item"
+      v-for="(question, index) in questions"
+      :key="question.id"
+    >
+      <h2>第{{ index + 1 }}题: {{ question.title }}</h2>
+      <img v-if="question.img" v-bind:src="question.img" class="tmImg" />
+      <div class="options">
+        <el-radio-group v-model="question.selected">
+          <el-radio
+            v-for="option in question.options"
+            :key="option.value"
+            :label="option.value"
+            :name="'question_' + question.id"
+          >
+            <!-- 若有img，就渲染img，若无就渲染label -->
+            <img v-if="option.img" :src="option.img" />
+            <span v-else style="font-size: 30px"
+              >{{ option.value }}:{{ option.label }}</span
+            >
+          </el-radio>
+        </el-radio-group>
+      </div>
+    </div>
+    <hr />
+  </div>
+  
+  `,
+  props: {
+    questions: {
+      type: Array,
+      required: true,
+    },
+  }
+};
