688b9d27884add642e9097e56f846d8cae4c6990
Nacog <88483952+Nacog@users.noreply.github.com>
2025-06-01 14:01:33 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -34,9 +34,9 @@
               <sst :items="questions.sst" />
        
               <el-button-group>
-                <el-button type="default" @click="pageSub" :icon="ArrowLeft" size="large" :disabled="currentPage===1">Last Part</el-button>
+                <!-- <el-button type="default" @click="pageSub" :icon="ArrowLeft" size="large" :disabled="currentPage===1">Last Part</el-button> -->
                 <el-button type="primary" @click="willSubmit" id="Submit" size="large">提交答案</el-button>
-                <el-button type="default" @click="pageAdd" size="large" :disabled="currentPage===4">Next Part<el-icon class="el-icon--right"><ArrowRight /></el-icon></el-button>
+                <!-- <el-button type="default" @click="pageAdd" size="large" :disabled="currentPage===4">Next Part<el-icon class="el-icon--right"><ArrowRight /></el-icon></el-button> -->
               </el-button-group>
 
 
@@ -67,6 +67,17 @@ import lxt from "@/components/lxt.vue";
 
 
 //实际使用中数据从后端获取
+
+//axios 获得题目
+// async function getTm(addr) {
+//   try{
+//     let response=await 
+//   }catch(e){
+//     console.error(e);
+//   }
+// }
+
+
 const mockQuestions={
   xzt: [
     {
@@ -353,7 +364,7 @@ export default {
       questions: mockQuestions,
       studentInfo: mockStudentInfo,
       corAnswers: mockCorAnswers,
-      currentPage:1 ,
+      // currentPage:1 ,
 
 
     };
@@ -394,10 +405,16 @@ export default {
 
       // 获得画图题答案
       totalTm+=this.questions.htt.userAnswer.length;
-      this.questions.htt.userAnswer.forEach(ans=>{
-        ans.forEach(e=>{
-          if(e!==null)
+      // 防止多次计算
+      let prev=[];
+      this.questions.htt.userAnswer.forEach((ans,index)=>{
+        ans.forEach((e)=>{
+          if(e!==null&&prev[index]==null)
+          {
             answeredCount++;
+            prev[index]=e;
+            // console.log(e+","+prev[index]+","+index);
+          }
         })
       })
 
@@ -542,18 +559,18 @@ export default {
     //   console.log(boolList);
     // },
 
-    pageAdd(){
-      if(this.currentPage<5) {
-        this.currentPage++;
-        document.documentElement.scrollTop = 0;
-      }
-    },
-    pageSub(){
-      if(this.currentPage>1) {
-        this.currentPage--;
-        document.documentElement.scrollTop = 0;
-      }
-    },
+    // pageAdd(){
+    //   if(this.currentPage<5) {
+    //     this.currentPage++;
+    //     document.documentElement.scrollTop = 0;
+    //   }
+    // },
+    // pageSub(){
+    //   if(this.currentPage>1) {
+    //     this.currentPage--;
+    //     document.documentElement.scrollTop = 0;
+    //   }
+    // },
     // 禁用右键菜单
     preventContextMenu(e) {
       ElMessage('为了更好的体验，右键已被禁用')
