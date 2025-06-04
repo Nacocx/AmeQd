771b4ccd5c5d8b54bb6ae42e56fedf601a47391d
Nacocx <88483952+Nacocx@users.noreply.github.com>
2025-06-04 21:37:28 +0800

    feat: mod

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteSingleFile(),
   ],
 
   base:"./",
@@ -21,5 +22,7 @@ export default defineConfig({
     // minify: false, // 禁用压缩（JS/CSS 保持可读）
     // sourcemap: true, // 生成 sourcemap（方便调试）
     // cssMinify: false,
+    cssCodeSplit: false, // 禁用CSS代码分割
+    assetsInlineLimit: 100000000 // 设置非常大的限制值，使所有资源内联
   },
 })
