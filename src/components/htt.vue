b01e7f6a81539acfbc058ae4731e3568898d5553
Nacog <88483952+Nacocx@users.noreply.github.com>
2025-06-08 14:52:42 +0800

    优化

--- src/components/htt.vue
+++ src/components/htt.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="htt_main_body">
-    <h1>{{ message.title }}:请拖进与图片物品数量一样的:<img :src="message.shape[message.id - 1]" alt="" class="shape">
+    <h1>{{ message.title }}请拖进与图片物品数量一样的:<img :src="message.shape[message.id - 1]" alt="" class="shape">
       <div class="questions">
 
       </div>
