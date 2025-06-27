f20645e255dffe4ec4e5995726c497925fd8de3a
suki2498 <3128486047@qq.com>
2025-06-27 15:45:26 +0800

    视频插入模板题

---
+++ TmJson/sx-01-s-01-01-01-vedio.js
@@ -0,0 +1,60 @@
+const vedio={
+     sst: [
+    {
+      id: 1,
+      title: { image: `${basePath}/sst1.png`, count: 2 },
+      tuXingPath: `${basePath}/sst_circle.png`,
+      userAnswer: "",
+    },
+    {
+      id: 2,
+      title: { image: `${basePath}/sst2.png`, count: 3 },
+      tuXingPath: `${basePath}/sst_circle.png`,
+      userAnswer: "",
+    },
+    {
+      id: 3,
+      title: { image: `${basePath}/sst3.png`, count: 4 },
+      tuXingPath: `${basePath}/sst_circle.png`,
+      userAnswer: "",
+    },
+    {
+      id: 4,
+      title: { image: `${basePath}/sst4.png`, count: 5 },
+      tuXingPath: `${basePath}/sst_circle.png`,
+      userAnswer: "",
+    },
+  ],
+  "qst_1": {
+    "title": "对照上面图片根据左边的图形数量圈出右边的数量",
+    "example": {
+      "t_img":`${basePath}/t1_23.png`,
+      "a_img": `${basePath}/a1_48.png`
+    },
+    "question": {
+      "t_img": `${basePath}/left1.png`,
+      "n_img":`${basePath}/right1.png`,
+      "num": 4,
+      "tureNum": 2
+    },
+    "img": `${basePath}/jian.png`,
+    "result": false,
+    "changed": false
+  },
+    "qst_2": {
+    "title": "对照上面图片根据左边的图形数量圈出右边的数量",
+    "example": {
+      "t_img":`${basePath}/t1_23.png`,
+      "a_img": `${basePath}/a1_48.png`
+    },
+    "question": {
+      "t_img": `${basePath}/left2.png`,
+      "n_img":`${basePath}/right2.png`,
+      "num": 8,
+      "tureNum": 3
+    },
+    "img": `${basePath}/jian.png`,
+    "result": false,
+    "changed": false
+  },
+}
